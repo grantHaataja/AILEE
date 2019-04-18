817b1e6a0bf9f7886885546edc82a471fb5c99f5
Misha Turnbull <mishaturnbull@gmail.com>
2019-04-18 18:24:14 -0500

    Start pep8 refactor
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/events/event1000000.py
+++ src/events/event1000000.py
@@ -2,8 +2,10 @@
 You ran a lot of commands.
 '''
 
+
 def check_run(*args, **kwargs):
     return len(kwargs['game'].history) == 1000000
+
 
 def run(*args, **kwargs):
     print("You just ran your millionth command. Nerd.")

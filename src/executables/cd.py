c6301f91ac2049c6bf564895e1d9376e97d6fb6e
Misha Turnbull <mishaturnbull@gmail.com>
2019-05-05 22:50:25 -0500

    Implement argparse in cd; redo cd command better
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/executables/cd.py
+++ src/executables/cd.py
@@ -2,29 +2,39 @@
 "Gradually being replaced by iPod"
 
 Description: Change directories (move from current folder to another folder) **see ls**
-
-Usage: cd directory_name
-
-Usage: cd .. (moves back up to the previous directory)
 """
+
+import argparse
+
+import filesystem
+
+parser = argparse.ArgumentParser(
+    prog='cd',
+    description=__doc__,
+    formatter_class=argparse.RawTextHelpFormatter,
+)
+parser.add_argument(
+    'directory',
+    default='.',
+    type=str,
+    help='target directory to move to',
+)
 
 
 def run(*args, **kwargs):
-    emptyList = True
-    for arg in args:
-        if arg:
-            emptyList = False
-    assert len(args) != 0 and not emptyList, \
-        "No directory specified.\n\nUsage: cd [Directory]"
-    assert len(args) == 1, "Invalid use of cd.\n\nUsage: cd [Directory]"
+
+    try:
+        data = parser.parse_args(args)
+    except SystemExit:
+        return
 
     shell = kwargs['shell']
 
-    assert args[0] == '.' or args[0] == '..' or '.' not in args[0], \
-        "Can't change directory into a file"
-
-    try:
-        new_cwd = shell.cwd[args[0]]
-        shell.cwd = new_cwd
-    except AssertionError:
+    if data.directory in list(shell.cwd.children.keys()):
+        new_cwd = shell.cwd[data.directory]
+        if isinstance(new_cwd, filesystem.Directory):
+            shell.cwd = new_cwd
+        else:
+            print("nope, that's a file")
+    else:
         print("nope, directory doesn't exist")
